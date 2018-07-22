e242c1a55e031ebba84e9a008f93c2b1ab46870d
Caleb Taylor <aquaductape@gmail.com>
2018-07-21 20:24:56 -0700

    added animations and combinations

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const stats = document.querySelector('.stats')
 
 function Player(name, shape, fill) {
   this.name = name
-  this.shape = shape
+  this.shape = svg[shape]
   this.score = 0
   this.turn = false
   this.fill = fill
@@ -23,17 +23,22 @@ let gameOver = false
 //Player One goes first
 player1.turn = true
 
+
+
 box.forEach((item) => {
   item.addEventListener('click', (e) => {
-    const row = e.target.parentNode.getAttribute('data-row');
+    const row = e.target.parentNode.getAttribute('data-row')
     const column = e.target.getAttribute('data-col')
+    const fill = e.target
+
+    const foo = { name: 'whatever'}
     if(gameOver) {
       return
     }
 
     console.log(row, column)
 
-    const player = fillBoard(row, column)
+    const player = fillBoard(row, column, fill)
     console.log(board)
 
     const animate = checkBoard(player)
@@ -43,13 +48,49 @@ box.forEach((item) => {
 
 function checkBoard(player) {
   // check row
-  for(let i = 0; i < board.length; i++) {
-    if(board[i].every(item => item === player.fill)) {
+  for(let row = 0; row < board.length; row++) {
+    if(board[row].every(item => item === player.fill)) {
       console.log(`${player.name} has won!`)
       gameOver = true
-      return `row ${i}`
+      return `row${row}`
+    }
+
+    // check column
+    let count = 0
+    for(let column = 0; column < board.length; column++) {
+      if(board[column][row] === player.fill) {
+        count++
+        if(count === board.length) {
+          console.log(`${player.name} has won!`)
+          gameOver = true
+        }
+      }
     }
   }
+
+  //check diagonal
+  let diagonal1 = 0
+  let diagonal2 = 0
+  // [0,0][1,1][2,2]
+  // [0,2][1,1][2,0]
+  for(let i = 0; i < board.length; i++) {
+    if(board[i][i] === player.fill) {
+      diagonal1++
+      if(diagonal1 === board.length) {
+        console.log(`${player.name} has won!`)
+        gameOver = true
+      }
+    }
+    
+    if(board[i][board.length - 1 -i] === player.fill) {
+      diagonal2++
+      if(diagonal2 === board.length) {
+        console.log(`${player.name} has won!`)
+        gameOver = true
+      }
+    }
+  }
+
 }
 
 function switchPlayers(player1, player2) {
@@ -61,7 +102,7 @@ function switchPlayers(player1, player2) {
   return player
 }
 
-function fillBoard(row, column) {
+function fillBoard(row, column, fill) {
   // if board already filled return nothing
   if(board[row][column] !== null) {
     return
@@ -69,13 +110,12 @@ function fillBoard(row, column) {
 
   const player = switchPlayers(player1, player2)
 
+  fill.innerHTML = player.shape
+
   console.log(player.name)
   
   board[row][column] = player.fill
 
-  return player
-}
 
-function kitty () {
-  console.log('Meowth thats me!')
+  return player
 }
