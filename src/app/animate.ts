1983ef7b5d8ada011ab5d91fe5a9c6e40fbf0fae
Caleb <aquaductape@gmail.com>
2019-12-04 14:56:28 -0800

    updated animation names

--- src/app/animate.ts
+++ src/app/animate.ts
@@ -5,15 +5,16 @@ const gameStart = <HTMLDivElement>document.querySelector(".game-start");
 const stats = document.querySelector(".stats");
 const box = document.querySelectorAll("[data-column]");
 
-export const animateLine = (animate: string | null) => {
-  if (!animate || !line) {
+export const animateLine = () => {
+  if (!line) {
     return null;
   }
+  const animate = data.winPosition;
 
-  if (animate === "diagTopLeft") {
+  if (animate === "DIAG_TOP_LEFT") {
     line.innerHTML = data.lines.lineLong;
   }
-  if (animate === "diagBotLeft") {
+  if (animate === "DIAG_BOT_LEFT") {
     line.innerHTML = data.lines.lineLong;
     const lineLong = <HTMLElement | null>line.querySelector(".line-long");
 
@@ -21,7 +22,7 @@ export const animateLine = (animate: string | null) => {
 
     lineLong.style.transform = "rotate(90deg)";
   }
-  if (animate === "row0") {
+  if (animate === "ROW_0") {
     line.innerHTML = data.lines.lineShort;
     const lineShort = <HTMLElement | null>line.querySelector(".line-short");
 
@@ -29,10 +30,10 @@ export const animateLine = (animate: string | null) => {
 
     lineShort.style.transform = "translateY(-33.4%)";
   }
-  if (animate === "row1") {
+  if (animate === "ROW_1") {
     line.innerHTML = data.lines.lineShort;
   }
-  if (animate === "row2") {
+  if (animate === "ROW_2") {
     line.innerHTML = data.lines.lineShort;
     const lineShort = <HTMLElement | null>line.querySelector(".line-short");
 
@@ -40,7 +41,7 @@ export const animateLine = (animate: string | null) => {
 
     lineShort.style.transform = "translateY(33.4%)";
   }
-  if (animate === "col0") {
+  if (animate === "COL_0") {
     line.innerHTML = data.lines.lineShort;
     const lineShort = <HTMLElement | null>line.querySelector(".line-short");
     const lineShortInner = <HTMLElement | null>(
@@ -53,7 +54,7 @@ export const animateLine = (animate: string | null) => {
     lineShortInner.style.transform = "translateY(33.4%)";
     "rotate(90deg)";
   }
-  if (animate === "col1") {
+  if (animate === "COL_1") {
     line.innerHTML = data.lines.lineShort;
     const lineShort = <HTMLElement | null>line.querySelector(".line-short");
 
@@ -62,7 +63,7 @@ export const animateLine = (animate: string | null) => {
     lineShort.style.transform = "rotate(90deg)";
   }
 
-  if (animate === "col2") {
+  if (animate === "COL_2") {
     line.innerHTML = data.lines.lineShort;
     const lineShort = <HTMLElement | null>line.querySelector(".line-short");
     const lineShortInner = <HTMLElement | null>(
@@ -78,33 +79,43 @@ export const animateLine = (animate: string | null) => {
   console.log(animate);
 };
 
+const cleanUp = (e: Event) => {
+  e.preventDefault();
+
+  box.forEach(item => {
+    item.setAttribute("aria-label", "empty");
+
+    const itemChild = item.firstElementChild;
+    if (itemChild) {
+      itemChild.innerHTML = "";
+    }
+  });
+
+  const player = data.player1.turn ? data.player1 : data.player2;
+  if (!stats || !line) return null;
+  stats.innerHTML = `Go ${player.name}!`;
+  line.innerHTML = "";
+
+  data.gameOver = false;
+  data.gameTie = false;
+  data.winPosition = "";
+
+  let count = 0;
+  data.board = data.board.map(item =>
+    item.map(item => {
+      const num = count;
+      count++;
+      return num;
+    })
+  );
+
+  gameStart.innerHTML = "";
+};
+
 export const addStartBtn = () => {
   gameStart.innerHTML = `<button class="btn btn-start">Play Again?</button>`;
 
   const btn = <HTMLDivElement>document.querySelector(".btn-start");
-
-  const cleanUp = (e: Event) => {
-    e.preventDefault();
-
-    box.forEach(item => {
-      item.setAttribute("aria-label", "empty");
-
-      const itemChild = item.firstElementChild;
-      if (itemChild) {
-        itemChild.innerHTML = "";
-      }
-    });
-
-    const player = data.player1.turn ? data.player1 : data.player2;
-    if (!stats || !line) return null;
-    stats.innerHTML = `Go ${player.name}!`;
-    line.innerHTML = "";
-
-    data.gameOver = false;
-    data.board = data.board.map(item => item.map(item => (item = null)));
-
-    gameStart.innerHTML = "";
-  };
 
   btn.addEventListener("click", cleanUp);
 };
