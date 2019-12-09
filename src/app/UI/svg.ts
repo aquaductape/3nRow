e6aa1edfcc87ede77237a620402395b630a00023
Caleb <aquaductape@gmail.com>
2019-12-09 10:32:11 -0800

    removed local defs inside svg icons and seperated them into seperate defs container which contains all the defs

--- src/app/UI/svg.ts
+++ src/app/UI/svg.ts
@@ -1,79 +1,50 @@
-const svg = {
+import { Isvg } from "../../models/index";
+
+const svg: Isvg = {
   circle: `
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100" height="100" viewBox="0 0 26.4583 26.4583" version="1.1" id="svg8">
-  <defs id="defs2">
-    <linearGradient id="linearGradient3789">
-      <stop id="stop3785" offset="0" stop-color="#ff0051" stop-opacity="1"/>
-      <stop id="stop3787" offset="1" stop-color="#ffc300" stop-opacity="1"/>
-    </linearGradient>
-    <linearGradient id="linearGradient3759">
-      <stop offset="0" id="stop3755" stop-color="#ff4900" stop-opacity="1"/>
-      <stop offset="1" id="stop3757" stop-color="#ffc300" stop-opacity="1"/>
-    </linearGradient>
-    <linearGradient xlink:href="#linearGradient3789" id="linearGradient3761" x1="13.8088" y1="247" x2="78.3068" y2="243.7928" gradientUnits="userSpaceOnUse" gradientTransform="matrix(0 .2635 -.2635 0 78.3119 .0543)"/>
-    <linearGradient xlink:href="#linearGradient3759" id="linearGradient3775" gradientUnits="userSpaceOnUse" gradientTransform="matrix(.2635 0 0 .2635 270.596 -51.853)" x1="13.8088" y1="247" x2="86.1912" y2="247"/>
-    <linearGradient y2="259.7645" x2="69.9669" y1="247" x1="13.8088" gradientTransform="matrix(0 .2635 .2635 0 -11.6131 -6.3685)" gradientUnits="userSpaceOnUse" id="linearGradient3736" xlink:href="#linearGradient3759"/>
-    <linearGradient y2="243.7928" x2="78.3068" y1="247" x1="13.8088" gradientTransform="matrix(0 .2635 -.2635 0 118.5518 -6.3685)" gradientUnits="userSpaceOnUse" id="linearGradient3756" xlink:href="#linearGradient3789"/>
-    <linearGradient xlink:href="#linearGradient3789" id="linearGradient841" x1="53.4693" y1="-1.5099" x2="53.4693" y2="13.7502" gradientUnits="userSpaceOnUse"/>
-    <linearGradient xlink:href="#linearGradient3759" id="linearGradient851" x1="53.5009" y1="-2.4148" x2="53.5009" y2="13.775" gradientUnits="userSpaceOnUse"/>
-  </defs>
   <g transform="translate(-40.2399 6.4228)" id="g3764">
-    <path d="m 53.469328,15.12263 a 8.316061,8.3162622 0 0 0 7.20192,-4.15814 8.316061,8.3162622 0 0 0 0,-8.3162599 8.316061,8.3162622 0 0 0 -7.20192,-4.15813" class="circle-left" id="path14" fill="none" fill-opacity=".9202" stroke="url(#linearGradient851)" stroke-width="2.6458" stroke-linecap="round" stroke-linejoin="miter" stroke-miterlimit="4" stroke-dasharray="none" stroke-opacity="1"/>
-    <path d="m 53.469338,15.12263 a 8.316061,8.3162622 0 0 1 -8.049378,-6.2271099 8.316061,8.3162622 0 0 1 4.005148,-9.35577 8.316061,8.3162622 0 0 1 10.061675,1.52651" class="circle-right" id="path16" opacity="1" fill="none" fill-opacity="1" stroke="url(#linearGradient841)" stroke-width="2.6458" stroke-linecap="round" stroke-linejoin="miter" stroke-miterlimit="4" stroke-dasharray="none" stroke-opacity="1"/>
+    <path d="m 53.469328,15.12263 a 8.316061,8.3162622 0 0 0 7.20192,-4.15814 8.316061,8.3162622 0 0 0 0,-8.3162599 8.316061,8.3162622 0 0 0 -7.20192,-4.15813" class="animate__circle-left" id="path14" fill="none" fill-opacity=".9202" stroke="url(#b)" stroke-width="2.6458" stroke-linecap="round" stroke-linejoin="miter" stroke-miterlimit="4" stroke-dasharray="none" stroke-opacity="1"/>
+    <path d="m 53.469338,15.12263 a 8.316061,8.3162622 0 0 1 -8.049378,-6.2271099 8.316061,8.3162622 0 0 1 4.005148,-9.35577 8.316061,8.3162622 0 0 1 10.061675,1.52651" class="animate__circle-right" id="path16" opacity="1" fill="none" fill-opacity="1" stroke="url(#b)" stroke-width="2.6458" stroke-linecap="round" stroke-linejoin="miter" stroke-miterlimit="4" stroke-dasharray="none" stroke-opacity="1"/>
   </g>
 </svg>
 `,
   cross: `
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100" height="100" viewBox="0 0 26.4583 26.4583" version="1.1" id="svg8">
-  <defs id="defs2">
-    <linearGradient id="linearGradient3790">
-      <stop id="stop3786" offset="0" stop-color="#0cf" stop-opacity="1"/>
-      <stop id="stop3788" offset="1" stop-color="#5fd" stop-opacity="1"/>
-    </linearGradient>
-    <linearGradient id="linearGradient3782">
-      <stop id="stop3778" offset="0" stop-color="#5fd" stop-opacity="1"/>
-      <stop id="stop3780" offset="1" stop-color="#0cf" stop-opacity="1"/>
-    </linearGradient>
-    <linearGradient id="linearGradient3789">
-      <stop id="stop3785" offset="0" stop-color="#ff0051" stop-opacity="1"/>
-      <stop id="stop3787" offset="1" stop-color="#ffc300" stop-opacity="1"/>
-    </linearGradient>
-    <linearGradient id="linearGradient3759">
-      <stop offset="0" id="stop3755" stop-color="#ff4900" stop-opacity="1"/>
-      <stop offset="1" id="stop3757" stop-color="#ffc300" stop-opacity="1"/>
-    </linearGradient>
-    <linearGradient xlink:href="#linearGradient3789" id="linearGradient3761" x1="13.8088" y1="247" x2="78.3068" y2="243.7928" gradientUnits="userSpaceOnUse" gradientTransform="matrix(0 .2635 -.2635 0 78.3119 .0543)"/>
-    <linearGradient xlink:href="#linearGradient3759" id="linearGradient3775" gradientUnits="userSpaceOnUse" gradientTransform="matrix(.2635 0 0 .2635 270.596 -51.853)" x1="13.8088" y1="247" x2="86.1912" y2="247"/>
-    <linearGradient gradientUnits="userSpaceOnUse" y2="19.9728" x2="6.4856" y1="13.2292" x1="13.2292" id="linearGradient3784" xlink:href="#linearGradient3782"/>
-    <linearGradient gradientUnits="userSpaceOnUse" y2="22.7431" x2="21.9646" y1="11.6221" x1="11.6221" id="linearGradient3792" xlink:href="#linearGradient3790"/>
-  </defs>
-  <g class="left-dot" fill="none" fill-opacity=".9202" stroke="#0cf" stroke-width="2.6458" stroke-linecap="round" stroke-linejoin="round">
+  <g class="animate__left-dot" fill="none" fill-opacity=".9202" stroke="#0cf" stroke-width="2.6458" stroke-linecap="round" stroke-linejoin="round">
     <path d="m 6.4855042,19.986408 a 9.5368996,9.5368996 0 0 1 5.62e-5,-0.03275"/>
   </g>
-  <g class="right-dot" fill="none" fill-opacity=".9202" stroke="#5fd" stroke-width="2.6458" stroke-linecap="round" stroke-linejoin="round">
+  <g class="animate__right-dot" fill="none" fill-opacity=".9202" stroke="#5fd" stroke-width="2.6458" stroke-linecap="round" stroke-linejoin="round">
     <path transform="scale(-1 1)" d="m -19.972774,19.953657 a 9.5368996,9.5368996 0 0 1 -1.1e-5,-0.01438" class="circle3768"/>
   </g>
   <g fill="none" stroke-width="2.6458" stroke-linecap="round">
-    <g stroke="url(#linearGradient3792)" transform="translate(0 -.0191)">
-      <path d="M 6.4855595,6.4855597 19.972772,19.972772" class="right-line"/>
+    <g stroke="url(#c)" transform="translate(0 -.0191)">
+      <path d="M 6.4855595,6.4855597 19.972772,19.972772" class="animate__right-line"/>
     </g>
-    <g stroke="url(#linearGradient3784)" transform="translate(0 -.0191)">
-      <path d="M 19.972772,6.4855597 6.4855597,19.972772" class="left-line"/>
+    <g stroke="url(#d)" transform="translate(0 -.0191)">
+      <path d="M 19.972772,6.4855597 6.4855597,19.972772" class="animate__left-line"/>
     </g>
   </g>
 </svg>
 `,
+  triangle: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 26.4583 26.4583" height="100" width="100">
+  <g fill="none" stroke-width="2.6458" stroke-linecap="round">
+    <path class="animate__first-line" d="M 21.666126,20.554903 H 4.792205" stroke="url(#e)"/>
+    <path class="animate__second-line" d="M 13.229166,5.941659 4.792205,20.554903" stroke="url(#f)"/>
+    <path class="animate__third-line" d="M 21.666125,20.554904 13.229166,5.941659" stroke="url(#g)"/>
+  </g>
+</svg>`,
   lineShort: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="svg8" version="1.1" viewBox="0 0 26.4583 26.4583" height="100" width="100">
   <defs id="defs2">
     <linearGradient id="linearGradient3722">
-      <stop id="stop3718" offset="0" stop-color="#2a7fff" stop-opacity="1"/>
-      <stop id="stop3720" offset="1" stop-color="#2ae4ff" stop-opacity="1"/>
+      <stop id="stop3718" class="line-color-primary" offset="0" stop-color="#2a7fff" stop-opacity="1"/>
+      <stop id="stop3720" class="line-color-secondary" offset="1" stop-color="#2ae4ff" stop-opacity="1"/>
     </linearGradient>
     <linearGradient gradientUnits="userSpaceOnUse" y2="283.7708" x2="25.5937" y1="283.7708" x1=".8646" id="linearGradient3724" xlink:href="#linearGradient3722"/>
     <linearGradient gradientTransform="rotate(45 10.9613 289.246)" y2="283.7708" x2="25.5937" y1="283.7708" x1=".8646" gradientUnits="userSpaceOnUse" id="linearGradient3738" xlink:href="#linearGradient3722"/>
   </defs>
-  <g class="line-short" style="transform-origin:13.2291665px 13.2291665px">
-    <g class="line-short-inner translate" style="transform-origin:13.2291665px 13.2291665px">
+  <g class="animate__line-short" style="transform-origin:13.2291665px 13.2291665px">
+    <g class="animate__line-short-inner translate" style="transform-origin:13.2291665px 13.2291665px">
       <path class="path3716" d="M 1.5355285,283.77082 H 24.922805" transform="translate(0 -270.5417)" fill="none" stroke="url(#linearGradient3724)" stroke-width="1.3229" stroke-linecap="round"/>
     </g>
   </g>
@@ -81,16 +52,44 @@ const svg = {
   lineLong: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="svg8" version="1.1" viewBox="0 0 26.4583 26.4583" height="100" width="100">
   <defs id="defs2">
     <linearGradient id="linearGradient3722">
-      <stop id="stop3718" offset="0" stop-color="#2a7fff" stop-opacity="1"/>
-      <stop id="stop3720" offset="1" stop-color="#2ae4ff" stop-opacity="1"/>
+      <stop id="stop3718" class="line-color-primary" offset="0" stop-color="#2a7fff" stop-opacity="1"/>
+      <stop id="stop3720" class="line-color-secondary" offset="1" stop-color="#2ae4ff" stop-opacity="1"/>
     </linearGradient>
     <linearGradient gradientUnits="userSpaceOnUse" y2="283.7708" x2="25.5937" y1="283.7708" x1=".8646" id="linearGradient3724" xlink:href="#linearGradient3722"/>
     <linearGradient gradientTransform="rotate(45 10.9613 289.246)" y2="283.7708" x2="25.5937" y1="283.7708" x1=".8646" gradientUnits="userSpaceOnUse" id="linearGradient3738" xlink:href="#linearGradient3722"/>
   </defs>
-  <g class="line-long" style="transform-origin:13.2291665px 13.2291665px">
+  <g class="animate__line-long" style="transform-origin:13.2291665px 13.2291665px">
     <path d="M 1.7532815,272.29493 24.705052,295.24671" class="path3726" transform="translate(0 -270.5417)" fill="none" stroke="url(#linearGradient3738)" stroke-width="1.3229" stroke-linecap="round"/>
   </g>
-</svg>`
+</svg>`,
+  defs: {
+    openingDef: `<svg xmlns="http://www.w3.org/2000/svg" height="0" width="0" viewBox="0 0 32 32">
+    <defs>`,
+    closingDef: `</defs></svg>`,
+    circle: `<linearGradient id="a">
+    <stop offset="0" class="color-primary" stop-color="#ff4900"/>
+    <stop offset="1" class="color-secondary" stop-color="#ffc300"/>
+  </linearGradient>
+  <linearGradient xlink:href="#a" id="b" x1="53.5009" y1="-2.4148" x2="53.5009" y2="13.775" gradientUnits="userSpaceOnUse"/>`,
+    cross: `
+  <linearGradient id="b">
+    <stop class="color-primary" offset="0" stop-color="currentColor"/>
+    <stop class="color-secondary" offset="1" stop-color="currentColor"/>
+  </linearGradient>
+  <linearGradient id="a">
+    <stop class="color-secondary" offset="0" stop-color="currentColor"/>
+    <stop class="color-primary" offset="1" stop-color="currentColor"/>
+  </linearGradient>
+  <linearGradient gradientUnits="userSpaceOnUse" y2="19.9728" x2="6.4856" y1="13.2292" x1="13.2292" id="d" xlink:href="#a"/>
+  <linearGradient gradientUnits="userSpaceOnUse" y2="22.7431" x2="21.9646" y1="11.6221" x1="11.6221" id="c" xlink:href="#b"/>`,
+    triangle: `<linearGradient id="d">
+      <stop class="color-primary" offset="0" stop-color="#0cf"/>
+      <stop class="color-secondary" offset="1" stop-color="#5fd"/>
+    </linearGradient>
+    <linearGradient y2="22.7431" x2="21.9646" y1="11.6221" x1="11.6221" gradientTransform="rotate(135 12.262 17.1198)" gradientUnits="userSpaceOnUse" id="e" xlink:href="#d"/>
+    <linearGradient y2="22.7431" x2="21.9646" y1="11.6221" x1="11.6221" gradientTransform="rotate(75 12.0032 10.372)" gradientUnits="userSpaceOnUse" id="f" xlink:href="#d"/>
+    <linearGradient y2="22.7431" x2="21.9646" y1="11.6221" x1="11.6221" gradientTransform="rotate(-165 15.6774 13.4011)" gradientUnits="userSpaceOnUse" id="g" xlink:href="#d"/>`
+  }
 };
 
 export default svg;
