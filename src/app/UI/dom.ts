7ecc9e27dec5b3cb157c2ff3fca3d977bb5a6c9e
Caleb <aquaductape@gmail.com>
2019-12-11 01:35:26 -0800

    changed few id to class, more content

--- src/app/UI/dom.ts
+++ src/app/UI/dom.ts
@@ -2,10 +2,7 @@ import svg from "./svg";
 
 const id = {
   btnPlayAgain: "btn-play_again",
-  dropDownDifficulty: "dropdown-difficulty",
-  btnDifficultyDDContainer: "btn-dropdown-difficulty",
-  btnAi: "btn-ai",
-  btnHuman: "btn-human",
+  checkboxAi: "checkbox-ai",
   gameStart: "game-start",
   P1BtnOptions: "P1-btn-options",
   P2BtnOptions: "P2-btn-options",
@@ -14,12 +11,18 @@ const id = {
   optionsResetScores: "options-reset-scores",
   optionsRestart: "options-restart"
 };
-const classes = {
+const className = {
   line: ".line-svg",
   stats: ".stats",
+  board: "board",
+  btnAi: "btn-ai",
+  btnHuman: "btn-human",
+  dropDownDifficulty: "dropdown-difficulty",
+  btnDifficultyDDContainer: "btn-dropdown-difficulty",
   dropDownSettings: ".dropdown-settings",
   playerBtnOptions: "player-btn-options",
   dropDownOptions: "dropdown-options",
+  optionsFriend: "options-friend",
   playerMark: "player-mark",
   playerOptions: "player-options",
   playerName: "player-name",
@@ -50,9 +53,9 @@ const query = {
 export const dom = {
   html: {
     btnPlayAgain: `<button id="${id.btnPlayAgain}" class="btn btn-primary btn-start">Play Again?</button>`,
-    btnBotDropdown: `<div id="${id.dropDownDifficulty}" class="dropdown hide-v"> <ul class="dropdown-settings"> <li class="dropdown-settings-list" tabindex="0">hard</li> <li class="dropdown-settings-list" tabindex="0">impossible</li> <li class="dropdown-settings-list" tabindex="0">cheater</li> </ul> </div>`,
+    btnBotDropdown: `<div  class="${className.dropDownDifficulty} dropdown hide-v"> <ul class="dropdown-settings"> <li class="dropdown-settings-list" tabindex="0">hard</li> <li class="dropdown-settings-list" tabindex="0">impossible</li> <li class="dropdown-settings-list" tabindex="0">cheater</li> </ul> </div>`,
     options: `<div class="dropdown-options %PLAYERID%-options"> <div class="dropdown-options-container"> <div class="options-shape"> <h2 class="options-title">Shape</h2> <hr> <ul class="shape-group"></ul> </div> <div class="options-color"> <h2 class="options-title">Color</h2> <hr> <ul class="color-group"></ul> </div> %AI% <div class="options-gameplay"> <hr> <button id="options-restart" class="btn btn-secondary options-btn">Restart</button> <button id="options-reset-scores" class="btn btn-secondary options-btn">Reset Scores</button> </div> </div> </div>`,
-    optionsAI: `<div class="options-ai"> <h2 class="options-title">AI</h2> <hr> <div id="btn-dropdown" class="btn-dropdown"><button id="btn-bot" class="btn btn-secondary options-btn">hard</button> <div class="dropdown hide-v"> <ul class="dropdown-settings"> <li class="dropdown-settings-list" tabindex="0">hard</li> <li class="dropdown-settings-list" tabindex="0">impossible</li> <li class="dropdown-settings-list" tabindex="0">cheater</li> </ul> </div> </div> <button class="btn btn-secondary options-btn">Apply</button> <button class="btn btn-secondary options-btn">Apply & Restart</button> </div>`
+    optionsAI: `<div class="options-ai"> <h2 class="options-title">Play Against ...</h2> <hr> <div class="btn-dropdown-difficulty btn-dropdown options-dropdown"><button class="btn-ai btn btn-secondary options-btn">AI Difficulty</button> </div><button class="options-friend btn btn-secondary options-btn">Friend</button>`
   },
   svg: {
     ...svg
@@ -61,7 +64,7 @@ export const dom = {
     ...id
   },
   class: {
-    ...classes
+    ...className
   },
   query: {
     ...query
