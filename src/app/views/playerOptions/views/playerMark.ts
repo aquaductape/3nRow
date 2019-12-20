0a8153e77ee25301370fe551b69c5624e0dd5dab
Caleb <aquaductape@gmail.com>
2019-12-19 23:10:19 -0800

    needed to add position absolute to full-color mark, since in some browsers it wasn't overlapped/hidden by the monochrome mark

--- src/app/views/playerOptions/views/playerMark.ts
+++ src/app/views/playerOptions/views/playerMark.ts
@@ -3,10 +3,11 @@ import { IPlayer } from "../../../../models/index";
 
 // renders two overlapping marks to hide top when active
 export const renderPlayerMark = (el: HTMLElement, player: IPlayer) => {
-  const normal = createHTMLFromString(player.svgMark);
+  const fullColor = createHTMLFromString(player.svgMark);
   const monochrome = createHTMLFromString(player.svgMark);
   monochrome.classList.add("monochrome");
+  fullColor.classList.add("full-color");
 
-  el.appendChild(normal);
+  el.appendChild(fullColor);
   el.appendChild(monochrome);
 };
