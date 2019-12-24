e0ea12096ce195fb5e5a3858d1daefde8693f21f
Caleb <aquaductape@gmail.com>
2019-12-24 01:16:03 -0800

    update deploy script

--- push_dist_to_gh-pages.sh
+++ push_dist_to_gh-pages.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "building dist...\n" &&
+echo -e "building dist...\n" &&
 npm run build &&
-echo "\nallow dist in gitingore\n"
+echo -e "\ntracking dist by removing it in gitingore\n"
 sed -i -- 's/dist//g' ./.gitignore &&
 git add . &&
-echo "\ncreating commit to push dist subtree for gh-pages...\n"
+echo -e "\ncommitting dist in order to push dist subtree for gh-pages...\n"
 git commit -m "update dist subtree for gh-pages" &&
 git push origin `git subtree split --prefix dist master`:gh-pages --force &&
 echo -e "\n removing dist commit, reseting to previous commit\n"
