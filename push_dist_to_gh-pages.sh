314ef280b4e007ee7b15a0499119c3bde82a665b
Caleb <aquaductape@gmail.com>
2020-03-30 18:55:06 -0700

    update text

--- push_dist_to_gh-pages.sh
+++ push_dist_to_gh-pages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "building dist...\n" &&
+echo -e "\nbuilding dist...\n" &&
 npm run build &&
 echo -e "\ntracking dist by removing it in gitingore\n"
 sed -i -- 's/dist//g' ./.gitignore &&
@@ -8,6 +8,6 @@ git add . &&
 echo -e "\ncommitting dist in order to push dist subtree for gh-pages...\n"
 git commit -m "update dist subtree for gh-pages" &&
 git push origin `git subtree split --prefix dist master`:gh-pages --force &&
-echo -e "\n removing dist commit, reseting to previous commit\n"
+echo -e "\nremoving dist commit, reseting to previous commit\n"
 git reset --hard HEAD~1
 echo -e "\nComplete!"
