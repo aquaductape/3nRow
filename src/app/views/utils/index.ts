70034a6f282c0b71a0d78e73bd3ae1ad27893ed6
Caleb Taylor <aquaductape@gmail.com>
2020-12-09 00:31:21 -0800

    new util

--- src/app/views/utils/index.ts
+++ src/app/views/utils/index.ts
@@ -209,3 +209,12 @@ export const convertObjPropsToHTMLAttr = ({
 
   return result;
 };
+
+export const hasAttributeValue = (
+  el: HTMLElement | Element,
+  { attr, val }: { attr: string; val: string }
+) => {
+  const attrResult = el.getAttribute(attr);
+  if (!attrResult) return false;
+  return attrResult === val;
+};
