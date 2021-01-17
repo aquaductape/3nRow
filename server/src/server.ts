36be7cd87d211c971b056e4a098df849a38e8250
Caleb Taylor <aquaductape@gmail.com>
2021-01-16 22:46:15 -0800

    update setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,16 @@
 import http from "http";
 import express from "express";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 import { Server } from "colyseus";
 import { Common } from "./rooms/common";
 
 const app = express();
 const port = Number(process.env.PORT || 3000);
+const corsOptions: CorsOptions = {
+  origin: process.env.ORIGIN_URL,
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const server = http.createServer(app);
@@ -19,4 +22,4 @@ gameServer.define("public", Common);
 gameServer.define("private", Common).filterBy(["password"]);
 gameServer.listen(port);
 
-// console.log(`Listening on ws://localhost:${port}`);
+console.log(`Listening on ws://localhost:${port}`);
--- server/webpack.config.js
+++ server/webpack.config.js
@@ -5,36 +5,10 @@ const path = require("path");
 const { NODE_ENV = "production" } = process.env;
 
 module.exports = {
-  // entry: "./src/server.ts",
-  // mode: "production",
-  // // watch: NODE_ENV === "development",
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.tsx?$/,
-  //       use: "ts-loader",
-  //       exclude: [/node_modules/],
-  //     },
-  //   ],
-  // },
-  // resolve: {
-  //   extensions: [".tsx", ".ts", ".js"],
-  // },
-  // output: {
-  //   filename: "index.js",
-  //   path: path.resolve(__dirname, "dist"),
-  // },
-  // externals: [nodeExternals()],
-  // plugins: [
-  //   // new WebpackShellPlugin({
-  //   //   onBuildEnd: ["yarn run:dev"],
-  //   // }),
-  //   new CleanWebpackPlugin(),
-  // ],
-
   entry: "./src/server.ts",
   target: "node",
   mode: "production",
+  watch: NODE_ENV === "development",
   module: {
     rules: [
       {
