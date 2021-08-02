f3a4cc4273f9ccbdfc449dfe3ac4ca9f1603a76f
Caleb Taylor <aquaductape@gmail.com>
2021-08-01 19:42:22 -0500

    update type

--- src/app/ts/index.ts
+++ src/app/ts/index.ts
@@ -3,11 +3,14 @@ export type TSkinProps = TSkin | TSkin[];
 export type TPosition = { row: number; column: number };
 
 // https://stackoverflow.com/a/51955852/8234457
-export type KnownKeys<T> = {
-  [K in keyof T]: string extends K ? never : number extends K ? never : K;
-} extends { [_ in keyof T]: infer U }
-  ? U
-  : never;
+// https://stackoverflow.com/a/68178028/8234457
+export type KnownKeys<T> = keyof {
+  [K in keyof T as string extends K
+    ? never
+    : number extends K
+    ? never
+    : K]: never;
+};
 
 // https://stackoverflow.com/a/55128956/8234457
 // can't use it, unwieldy
